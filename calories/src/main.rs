5d79095ed4e75ddeb1e9c2939a7ffa08446d8aee
Ivan Petrakov <rebyatyonok@yandex.com>
2022-12-01 16:07:44 +0300

    advent 01 done

---
+++ calories/src/main.rs
@@ -0,0 +1,53 @@
+use std::cmp;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("calories/src/input.txt").unwrap();
+
+    println!("{}", most_calories(&file));
+    println!("{:?}", top_three_sum(&file));
+}
+
+fn most_calories(file: &String) -> i32 {
+    let mut most = 0;
+    let mut current = 0;
+
+    for line in file.lines() {
+        if line.len() > 0 {
+            current += line.parse::<i32>().unwrap_or(0);
+        } else {
+            most = cmp::max(most, current);
+            current = 0;
+        }
+    }
+
+    most
+}
+
+fn top_three_sum(file: &String) -> i32 {
+    let mut sum = 0;
+    let mut result = vec![0, 0, 0];
+
+    file.lines().for_each(|line| {
+        if line.len() > 0 {
+            sum += line.parse::<i32>().unwrap_or(0);
+        } else {
+            result = result
+                .iter()
+                .map(|x| {
+                    if sum > *x {
+                        let tmp = sum;
+                        sum = 0;
+                        tmp
+                    } else {
+                        *x
+                    }
+                })
+                .collect();
+
+            sum = 0
+        }
+    });
+
+    result.iter().sum()
+}
