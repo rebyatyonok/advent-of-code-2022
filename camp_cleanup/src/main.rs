d03460d60fa1875fb3178cf5b06611233b2e7195
Ivan Petrakov <rebyatyonok@yandex.com>
2022-12-07 21:54:58 +0300

    camp cleanup second task is done

--- camp_cleanup/src/main.rs
+++ camp_cleanup/src/main.rs
@@ -1,13 +1,23 @@
 use std::fs;
 
-fn contains(first: (i32, i32), second: (i32, i32)) -> bool {
-    let (a, b) = first;
-    let (c, d) = second;
+type TRange = (i32, i32);
+type TRanges = (TRange, TRange);
 
-    (a <= c && b >= d) || (c <= a && d >= b)
+fn contains(first: &TRange, second: &TRange) -> bool {
+    let (x1, x2) = first;
+    let (y1, y2) = second;
+
+    (x1 <= y1 && x2 >= y2) || (y1 <= x1 && y2 >= x2)
 }
 
-fn to_ranges(vec: &[i32]) -> ((i32, i32), (i32, i32)) {
+fn overlaps(first: &TRange, second: &TRange) -> bool {
+    let (x1, x2) = first;
+    let (y1, y2) = second;
+
+    x1 <= y2 && y1 <= x2
+}
+
+fn to_ranges(vec: &[i32]) -> TRanges {
     if vec.len() != 4 {
         panic!("Wrong length of parsed values!")
     } else {
@@ -15,18 +25,22 @@ fn to_ranges(vec: &[i32]) -> ((i32, i32), (i32, i32)) {
     }
 }
 
-fn first_task(file: &String) -> usize {
-    file.lines()
-        .filter(|line| {
-            let parsed_line = line
-                .split(',')
-                .map(|range| range.split('-'))
-                .flat_map(|x| x.map(|e| e.parse::<i32>().unwrap()))
-                .collect::<Vec<i32>>();
+fn first_task(ranges: &Vec<TRanges>) -> usize {
+    ranges
+        .iter()
+        .filter(|x| {
+            let (f, s) = x;
+            contains(f, s)
+        })
+        .count()
+}
 
-            let (first, second) = to_ranges(&parsed_line);
-
-            contains(first, second)
+fn second_task(ranges: &Vec<TRanges>) -> usize {
+    ranges
+        .iter()
+        .filter(|x| {
+            let (f, s) = x;
+            overlaps(f, s)
         })
         .count()
 }
@@ -34,5 +48,15 @@ fn first_task(file: &String) -> usize {
 fn main() {
     let file = fs::read_to_string("camp_cleanup/src/input.txt").unwrap();
 
-    println!("{}", first_task(&file))
+    let parsed_lines = file.lines().map(|line| {
+        line.split(',')
+            .map(|range| range.split('-'))
+            .flat_map(|x| x.map(|e| e.parse::<i32>().unwrap()))
+            .collect::<Vec<i32>>()
+    });
+
+    let lines_as_ranges = parsed_lines.map(|x| to_ranges(&x)).collect();
+
+    println!("{}", first_task(&lines_as_ranges));
+    println!("{}", second_task(&lines_as_ranges));
 }
