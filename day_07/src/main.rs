2d55d666fe3dce354dcf771423b98a4e59b73c2a
Ivan Petrakov <rebyatyonok@yandex.com>
2022-12-16 13:22:03 +0300

    day_07 task is done

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,6 +1,4 @@
-use std::fs::File;
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 enum Node {
     File(usize),
     Directory(usize),
@@ -10,13 +8,12 @@ enum Command<'a> {
     Cd(&'a str),
     Ls,
     AddDir(&'a str),
-    AddFile(usize),
+    AddFile(usize, &'a str),
 }
 
 #[derive(Debug)]
 struct Directory {
-    name: String,
-    children: Vec<Node>,
+    children: std::collections::HashMap<String, Node>,
     parent: usize,
 }
 
@@ -29,9 +26,8 @@ struct FileSystem {
 impl FileSystem {
     fn new() -> FileSystem {
         let root = Directory {
-            children: vec![],
+            children: Default::default(),
             parent: 0,
-            name: String::from("root"),
         };
 
         FileSystem {
@@ -44,30 +40,40 @@ impl FileSystem {
         self.current = match to {
             "/" => 0,
             ".." => self.directories[self.current].parent,
-            name => match self.directories.iter().position(|e| e.name == name) {
-                Some(index) => index,
-                None => panic!("Can't find a dir in {:?} to {}", self.directories, name),
-            },
+            name => self.directories[self.current]
+                .children
+                .get(name)
+                .and_then(|node| match node {
+                    Node::File { .. } => panic!("Trying to cd to file with name {}", name),
+                    Node::Directory(dir) => Some(*dir),
+                })
+                .unwrap(),
+            // name => match self.directories.iter().position(|e| e.name == name) {
+            //     Some(index) => index,
+            //     None => panic!("Can't find a dir in {:?} to {}", self.directories, name),
+            // },
         }
     }
 
-    fn add_file(&mut self, size: usize) {
+    fn add_file(&mut self, size: usize, name: &str) {
         self.directories[self.current]
             .children
-            .push(Node::File(size));
+            .entry(name.to_string())
+            .or_insert_with(|| Node::File(size));
     }
 
     fn add_directory(&mut self, name: &str) {
         let directory = Directory {
-            name: name.to_string(),
-            children: vec![],
+            children: Default::default(),
             parent: self.current,
         };
-        let len = self.directories.len();
         self.directories.push(directory);
+        let index = self.directories.len() - 1;
+
         self.directories[self.current]
             .children
-            .push(Node::Directory(len + 1));
+            .entry(name.to_string())
+            .or_insert_with(|| Node::Directory(index));
     }
 }
 
@@ -83,35 +89,30 @@ fn parse_line(line: &str) -> Command {
         }
         ("$", "ls") => Command::Ls,
         ("dir", dirname) => Command::AddDir(dirname),
-        (size, _) => {
+        (size, name) => {
             let size = size.parse::<usize>().expect("Expected parsable string");
-            return Command::AddFile(size);
+            return Command::AddFile(size, name);
         }
     }
 }
 
-fn get_dir_sizes(fs: &FileSystem) -> usize {
-    let mut sizes = Vec::with_capacity(fs.directories.len());
-    sizes.resize(fs.directories.len(), 0);
+fn get_dir_size(dir: &Directory, fs: &FileSystem) -> usize {
+    dir.children
+        .iter()
+        .map(|(_, child)| match child {
+            &Node::File(size) => size,
+            &Node::Directory(index) => get_dir_size(&fs.directories[index], fs),
+        })
+        .sum()
+}
 
+fn get_dir_sizes(fs: &FileSystem) -> Vec<usize> {
     fs.directories
         .iter()
         .enumerate()
         .rev()
-        .for_each(|(i, dir)| {
-            let dir_size = dir
-                .children
-                .iter()
-                .map(|child| match child {
-                    &Node::File(size) => size,
-                    &Node::Directory(index) => sizes[index],
-                })
-                .sum();
-
-            sizes[i] = dir_size;
-        });
-
-    sizes.iter().sum()
+        .map(|(_, dir)| get_dir_size(dir, fs))
+        .collect()
 }
 
 fn main() {
@@ -125,9 +126,13 @@ fn main() {
             Command::Cd(path) => fs.cd(path),
             Command::Ls => (),
             Command::AddDir(dirname) => fs.add_directory(dirname),
-            Command::AddFile(size) => fs.add_file(size),
+            Command::AddFile(size, name) => fs.add_file(size, name),
         }
     }
 
-    println!("{:#?}", get_dir_sizes(&fs));
+    let limit = 100000 as usize;
+
+    let first_task: usize = get_dir_sizes(&fs).into_iter().filter(|e| e < &limit).sum();
+
+    println!("{:#?}", first_task);
 }
