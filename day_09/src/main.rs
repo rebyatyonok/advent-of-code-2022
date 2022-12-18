50e1468f1e533cbaf7b1b29cfe6475f75e329e85
Ivan Petrakov <rebyatyonok@yandex.com>
2022-12-19 00:23:22 +0300

    day 9 first task is done

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, collections::HashSet};
 
 #[derive(Debug, PartialEq, Eq)]
 enum Motion {
@@ -8,30 +8,34 @@ enum Motion {
     Right,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 struct Position {
     x: i32,
     y: i32,
 }
 
 struct Rope {
-    tail_motions: Vec<Position>,
+    tail_motions: HashSet<Position>,
     head_pos: Position,
     tail_pos: Position,
+    count: i32,
 }
 
 impl Rope {
     fn new() -> Rope {
+        let tail_pos = Position { x: 0, y: 0 };
         Rope {
-            tail_motions: vec![],
+            tail_motions: HashSet::from([tail_pos]),
             head_pos: Position { x: 0, y: 0 },
-            tail_pos: Position { x: 0, y: 0 },
+            tail_pos,
+            count: 0,
         }
     }
 
     fn move_head(&mut self, motion: &Motion, amount: i32) {
-        // println!("=== {:?} {} ===", direction, amount);
         for _ in 0..amount {
+            self.count += 1;
+
             match motion {
                 Motion::Up => self.head_pos.y += 1,
                 Motion::Down => self.head_pos.y -= 1,
@@ -39,61 +43,57 @@ impl Rope {
                 Motion::Right => self.head_pos.x += 1,
             }
 
-            // println!("Head {:?}", self.head_pos);
-
-            if !self.is_touching() {
-                self.adjust_tail(motion);
+            if !self.are_ends_touching() {
+                self.move_tail(motion);
             }
         }
     }
 
-    fn adjust_tail(&mut self, head_motion: &Motion) {
-        let mut new = self.tail_pos;
+    fn move_tail(&mut self, head_motion: &Motion) {
+        let mut new_position = self.tail_pos;
 
         match head_motion {
             Motion::Up | Motion::Down => {
                 if *head_motion == Motion::Up {
-                    new.y += 1;
+                    new_position.y += 1;
                 } else {
-                    new.y -= 1;
+                    new_position.y -= 1;
                 }
 
-                match self.head_pos.x.cmp(&new.x) {
-                    Ordering::Greater => new.x += 1,
-                    Ordering::Less => new.x -= 1,
+                match self.head_pos.x.cmp(&new_position.x) {
+                    Ordering::Greater => new_position.x += 1,
+                    Ordering::Less => new_position.x -= 1,
                     Ordering::Equal => (),
                 }
             }
             Motion::Left | Motion::Right => {
                 if *head_motion == Motion::Left {
-                    new.x -= 1;
+                    new_position.x -= 1;
                 } else {
-                    new.x += 1;
+                    new_position.x += 1;
                 }
 
-                match self.head_pos.y.cmp(&new.y) {
-                    Ordering::Greater => new.y += 1,
-                    Ordering::Less => new.y -= 1,
+                match self.head_pos.y.cmp(&new_position.y) {
+                    Ordering::Greater => new_position.y += 1,
+                    Ordering::Less => new_position.y -= 1,
                     Ordering::Equal => (),
                 }
             }
         }
 
-        self.tail_pos = new;
-        self.tail_motions.push(new);
+        self.tail_pos = new_position;
+        self.tail_motions.insert(new_position);
     }
 
-    fn is_touching(&self) -> bool {
-        let x_diff = (self.head_pos.x - self.tail_pos.x).abs();
-        let y_diff = (self.head_pos.y - self.tail_pos.y).abs();
+    fn are_ends_touching(&self) -> bool {
+        let dx = (self.head_pos.x - self.tail_pos.x).abs();
+        let dy = (self.head_pos.y - self.tail_pos.y).abs();
 
-        // println!("x_diff {} y_diff {}", x_diff, y_diff);
-
-        (x_diff <= 1) && (y_diff <= 1)
+        (dx <= 1) && (dy <= 1)
     }
 }
 
-fn line_to_motion(line: &str) -> (Motion, i32) {
+fn line_to_motion_amount(line: &str) -> (Motion, i32) {
     let mut tokens = line.split_whitespace();
     let direction = tokens.next().expect("No direction!");
     let amount = tokens.next().expect("No amount").parse::<i32>().unwrap();
@@ -114,10 +114,11 @@ fn main() {
     let mut rope = Rope::new();
 
     file.lines()
-        .map(line_to_motion)
+        .map(line_to_motion_amount)
         .for_each(|(motion, amount)| {
             rope.move_head(&motion, amount);
         });
 
     println!("{:?}", rope.tail_motions.len());
+    println!("{:?}", rope.count);
 }
