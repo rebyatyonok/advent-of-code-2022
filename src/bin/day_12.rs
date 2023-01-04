c5dbdf997913b7334ab3cfba153ad5b71d740cc9
Ivan Petrakov <rebyatyonok@yandex.com>
2023-01-05 00:55:32 +0300

    day 12 algo reworked

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,112 +1,155 @@
-use std::collections::HashSet;
+use std::{
+    collections::{BinaryHeap, HashMap, HashSet},
+    hash::Hash,
+};
 
-type Visited = HashSet<String>;
-type Matrix = Vec<Vec<char>>;
-
-#[derive(Clone, Copy)]
+#[derive(Eq, PartialEq, Clone, Copy, Hash, Debug)]
 struct Point {
     x: usize,
     y: usize,
+    priority: i32,
+    value: u8,
 }
 
-impl std::fmt::Display for Point {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "x:{} y:{}", self.x, self.y)
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.value.cmp(&other.value))
     }
 }
 
-fn is_valid(current: Point, next: Point, matrix: &Matrix, visited: &mut Visited) -> bool {
-    let next_char = matrix[next.y][next.x];
-    let current_char = matrix[current.y][current.x];
-
-    let is_visited = visited.get(&next.to_string()).is_some();
-    let is_next_letter_okay = if current_char == 'S' && next_char == 'a' {
-        true
-    } else if next_char > current_char {
-        next_char as u8 == current_char as u8 + 1
-    } else if next_char == 'E' {
-        current_char == 'z'
-    } else {
-        true
-    };
-
-    is_next_letter_okay && !is_visited
+impl Ord for Point {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
 }
 
-fn find_path(
-    current: Point,
-    matrix: &Matrix,
-    to: char,
-    visited: &mut Visited,
-    path: &mut Vec<char>,
-) -> bool {
-    let x = current.x;
-    let y = current.y;
-
-    visited.insert(current.to_string());
-
-    if y >= matrix.len() || x >= matrix[0].len() {
-        return false;
-    }
-
-    let mut next_coordinates = vec![];
-
-    if x + 1 < matrix[0].len() {
-        next_coordinates.push((x + 1, y))
-    }
-    if y + 1 < matrix.len() {
-        next_coordinates.push((x, y + 1))
-    }
-    if x > 0 {
-        next_coordinates.push((x - 1, y))
-    }
-    if y > 0 {
-        next_coordinates.push((x, y - 1));
-    }
-
-    if matrix[current.y][current.x] == to {
-        println!("Found E in {} {}", current.x, current.y);
-        return true;
-    }
-
-    for next in next_coordinates {
-        let point = Point {
-            x: next.0,
-            y: next.1,
-        };
-
-        if is_valid(current, point, matrix, visited) && find_path(point, matrix, to, visited, path)
-        {
-            path.push(matrix[current.y][current.x]);
-            return true;
+impl Point {
+    fn new(x: usize, y: usize, char: char) -> Self {
+        Point {
+            x,
+            y,
+            priority: 0,
+            value: char_to_value(char),
         }
     }
-
-    false
 }
 
-fn find_char(matrix: &[Vec<char>], char: char) -> Option<Point> {
-    for (y, line) in matrix.iter().enumerate() {
-        for (x, _) in line.iter().enumerate() {
-            if matrix[y][x] == char {
-                return Some(Point { x, y });
+struct Grid {
+    grid: Vec<Vec<Point>>,
+}
+
+impl Grid {
+    fn new(file: String) -> Grid {
+        let grid = file
+            .lines()
+            .enumerate()
+            .map(|(y, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(|(x, char)| Point::new(x, y, char))
+                    .collect()
+            })
+            .collect();
+
+        Grid { grid }
+    }
+
+    fn find(&self, char: char) -> Option<Point> {
+        for row in self.grid.iter() {
+            for point in row.iter() {
+                if point.value == char_to_value(char) {
+                    return Some(*point);
+                }
+            }
+        }
+
+        None
+    }
+
+    fn get_start_position(&self) -> Point {
+        self.find('S').unwrap()
+    }
+
+    fn get_end_position(&self) -> Point {
+        self.find('E').unwrap()
+    }
+
+    fn get_neighbors(&self, point: &Point) -> Vec<Point> {
+        let x = point.x;
+        let y = point.y;
+        let mut variants = vec![(x, y + 1), (x + 1, y)];
+
+        if y > 0 {
+            variants.push((x, y - 1));
+        }
+        if x > 0 {
+            variants.push((x - 1, y));
+        }
+
+        variants
+            .into_iter()
+            .filter_map(|(x, y)| match self.grid.get(y) {
+                None => None,
+                Some(row) => row.get(x).copied(),
+            })
+            .collect()
+    }
+}
+
+fn get_path(start: &Point, end: &Point, grid: &Grid) -> Vec<Point> {
+    let mut came_from = HashMap::new();
+    let mut frontier = BinaryHeap::new();
+    let mut visited: HashSet<Point> = HashSet::new();
+
+    frontier.push(*start);
+    came_from.insert(*start, None);
+
+    while let Some(current) = frontier.pop() {
+        let neighbors = grid.get_neighbors(&current);
+
+        for next in neighbors.into_iter() {
+            let is_visited = visited.get(&next).is_some();
+            let is_value_ok = if next >= current {
+                next.value.abs_diff(current.value) <= 1
+            } else {
+                true
+            };
+
+            if !is_visited && is_value_ok {
+                frontier.push(next);
+                came_from.insert(next, Some(current));
+                visited.insert(next);
             }
         }
     }
 
-    None
+    let mut current = *end;
+    let mut path = vec![];
+
+    while current != *start {
+        path.push(current);
+        current = came_from[&current].unwrap();
+    }
+
+    path
 }
 
 fn main() {
     let file = advent_of_code_2022::get_input_file();
-    let matrix: Vec<Vec<char>> = file.lines().map(|line| line.chars().collect()).collect();
-    let mut path = vec![];
-    let mut visited = HashSet::new();
+    let grid = Grid::new(file);
 
-    let start = find_char(&matrix, 'S').expect("Expected to have an S char in input");
+    let start = grid.get_start_position();
+    let end = grid.get_end_position();
 
-    find_path(start, &matrix, 'E', &mut visited, &mut path);
-    println!("{:?}", visited);
+    let path = get_path(&start, &end, &grid);
 
     println!("{:?}", path.len());
+}
+
+fn char_to_value(char: char) -> u8 {
+    match char {
+        'S' => 0,
+        'E' => 27,
+        char => char as u8 - 96,
+    }
 }
